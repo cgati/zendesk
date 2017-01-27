ed6c2e430ab67ac2afa78675fee16f41dc5744c0
Christian Gati <christian.gati@gmail.com>
2017-01-27 01:15:27 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications,
+        warnings)]
+//! # zendesk
+//!
+//! The `zendesk` crate provides an interface with Zendesk's API.
